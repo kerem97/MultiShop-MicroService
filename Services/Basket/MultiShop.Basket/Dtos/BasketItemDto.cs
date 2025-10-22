d84000b3f74a648b25d3376383235840582633b2
Kerem DURAK <kerem.durak@yahoo.com>
2025-10-22 17:15:07 +0300

    redis

---
+++ Services/Basket/MultiShop.Basket/Dtos/BasketItemDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiShop.Basket.Dtos
+{
+    public class BasketItemDto
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
