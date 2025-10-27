031739abcfc03109d226f3456923a55ad585c3ad
Kerem DURAK <kerem.durak@yahoo.com>
2025-10-27 17:22:01 +0300

    UI started

---
+++ Frontends/MultiShop.WebUI/ViewComponents/UILayoutViewComponents/_UILayoutNavbarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.UILayoutViewComponents
+{
+    public class _UILayoutNavbarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
